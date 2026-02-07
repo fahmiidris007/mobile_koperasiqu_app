bd7dd8a17669a89a3a071cf58985dc90990b3846
Fahmi Idris <fahmiidrismz@gmail.com>
2026-02-07 07:49:16 +0700

    fix - local format date

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'src/core/router/app_router.dart';
 import 'src/core/theme/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize date formatting for Indonesian locale
+  await initializeDateFormatting('id_ID', null);
 
   // Set system UI overlay style
   SystemChrome.setSystemUIOverlayStyle(
